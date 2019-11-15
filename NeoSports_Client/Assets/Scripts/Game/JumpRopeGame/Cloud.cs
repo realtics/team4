2145c60171a529a18d0a23fba06fa1f6d65067c1
Jiheon Kim <rlawys4565@gmail.com>
2019-11-15 14:30:46 +0900

    Client_JumpRopeGame(Optimazation/Namespace)
    - JumpRopeGame(줄 넘기 게임)에 네임스페이스 추가
    - JumpRopeGameManager, GetComponent 스크립트 최적화

--- NeoSports_Client/Assets/Scripts/Game/JumpRopeGame/Cloud.cs
+++ NeoSports_Client/Assets/Scripts/Game/JumpRopeGame/Cloud.cs
@@ -2,73 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cloud : MonoBehaviour
+namespace JumpRopeGame
 {
 
-	readonly Rect CloudShowRect = new Rect(-10.0f, -1.0f, 20.0f, 2.0f);
-
-	const string CloudSpriteNamePrefix = "scene_0";
-	const string CloudSpriteNameSuffix = "_cloud";
-	const int CloudSpriteIndexMin = 1;
-	const int CloudSpriteIndexMax = 4;
-
-	const float MoveSpeedMin = 0.5f;
-	const float MoveSpeedMax = 1.5f;
-	const float Depth = -1.0f;
-
-	Vector3 _position;
-	float _moveSpeed;
-
-	private void Start()
+	public class Cloud : MonoBehaviour
 	{
-		_position = transform.localPosition;
 
-		SetMoveSpeed();
-		SetRandomStartPosition();
-		SetRandomSprite();
-	}
+		readonly Rect CloudShowRect = new Rect(-10.0f, -1.0f, 20.0f, 2.0f);
 
-	private void Update()
-	{
-		Move();
-		CheckCanDestroy();
-	}
+		const string	CloudSpriteNamePrefix = "scene_0";
+		const string	CloudSpriteNameSuffix = "_cloud";
+		const int		CloudSpriteIndexMin = 1;
+		const int		CloudSpriteIndexMax = 4;
 
-	void Move()
-	{
-		_position.x += _moveSpeed * Time.deltaTime;
+		const float MoveSpeedMin = 0.5f;
+		const float MoveSpeedMax = 1.5f;
+		const float Depth = -1.0f;
 
-		transform.localPosition = _position;
-	}
+		Vector3	_position;
+		float	_moveSpeed;
 
-	void SetMoveSpeed()
-	{
-		_moveSpeed = Random.Range(MoveSpeedMin, MoveSpeedMax);
-	}
-
-	void SetRandomStartPosition() {
-		_position.x = CloudShowRect.xMin;
-		_position.y = Random.Range(CloudShowRect.yMin, CloudShowRect.yMax);
-		_position.z = Depth;
-
-		transform.localPosition = _position;
-	}
-
-	void SetRandomSprite()
-	{
-		int spriteIndex = Random.Range(CloudSpriteIndexMin, CloudSpriteIndexMax);
-		string spriteName = CloudSpriteNamePrefix + spriteIndex.ToString() + CloudSpriteNameSuffix;
-
-		Sprite sprite = Singleton<ResourceManager>.Instance.GetGameSprite(spriteName);
-		transform.GetComponent<SpriteRenderer>().sprite = sprite;
-	}
-
-	void CheckCanDestroy()
-	{
-		if(_position.x > CloudShowRect.xMax)
+		private void Start()
 		{
-			Destroy(gameObject);
+			_position = transform.localPosition;
+
+			SetMoveSpeed();
+			SetRandomStartPosition();
+			SetRandomSprite();
 		}
+
+		private void Update()
+		{
+			Move();
+			CheckCanDestroy();
+		}
+
+		void Move()
+		{
+			_position.x += _moveSpeed * Time.deltaTime;
+
+			transform.localPosition = _position;
+		}
+
+		void SetMoveSpeed()
+		{
+			_moveSpeed = Random.Range(MoveSpeedMin, MoveSpeedMax);
+		}
+
+		void SetRandomStartPosition()
+		{
+			_position.x = CloudShowRect.xMin;
+			_position.y = Random.Range(CloudShowRect.yMin, CloudShowRect.yMax);
+			_position.z = Depth;
+
+			transform.localPosition = _position;
+		}
+
+		void SetRandomSprite()
+		{
+			int spriteIndex = Random.Range(CloudSpriteIndexMin, CloudSpriteIndexMax);
+			string spriteName = CloudSpriteNamePrefix + spriteIndex.ToString() + CloudSpriteNameSuffix;
+
+			Sprite sprite = Singleton<ResourceManager>.Instance.GetGameSprite(spriteName);
+			transform.GetComponent<SpriteRenderer>().sprite = sprite;
+		}
+
+		void CheckCanDestroy()
+		{
+			if (_position.x > CloudShowRect.xMax)
+			{
+				Destroy(gameObject);
+			}
+		}
+
 	}
 
 }
