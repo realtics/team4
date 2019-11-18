b3ff3adfa1da45ddf3762f83821f45227003bf2c
Jiheon Kim <rlawys4565@gmail.com>
2019-11-18 10:02:18 +0900

    Client_스크립트 디렉토리 정리
    - 일관성 없는 스크립트 디렉토리 위치 조정

---
+++ NeoSports_Client/Assets/Scripts/Effect/BackgroundManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : Singleton<BackgroundManager>
+{
+	const string PrefabCloudPath = "Prefabs/JumpRopeGame/Prefab_Cloud";
+
+	const float GenerateCloudIntervalMin = 1.0f;
+	const float GenerateCloudIntervalMax = 5.0f;
+
+	float GenerateCloudCycle = 0.0f;
+	float GenerateCloudTimer = 0.0f;
+
+	GameObject prefCloud;
+
+	public GameObject BackgroundCloudGroup;
+
+	private void Awake()
+	{
+		prefCloud = Resources.Load<GameObject>(PrefabCloudPath);
+	}
+
+	private void Update()
+	{
+		GenerateCloud();
+	}
+
+	void GenerateCloud()
+	{
+		GenerateCloudTimer += Time.deltaTime;
+
+		if (GenerateCloudTimer > GenerateCloudCycle)
+		{
+			GenerateCloudTimer -= GenerateCloudCycle;
+			GenerateCloudCycle = Random.Range(GenerateCloudIntervalMin, GenerateCloudIntervalMax);
+			Instantiate(prefCloud, BackgroundCloudGroup.transform);
+		}
+	}
+
+}
