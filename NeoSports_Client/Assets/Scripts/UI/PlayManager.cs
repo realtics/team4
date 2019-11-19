4430ff1557a147450b4fdb87b57e8aa8dc5dcd2c
Jiheon Kim <rlawys4565@gmail.com>
2019-11-19 16:41:20 +0900

    Client_메인 메뉴(게임 선택 창)
    - 단순 버튼 형식에서 Scroll View를 이용해 싱글 플레잉와 멀티 플레이를 선택하는 방식으로 바꿈
    - 각 싱글, 멀티 버튼에 씬 변경 기능 추가
    - 현재 멀티 플레이 버튼은 Disable로 비활성화 되어 있음

--- NeoSports_Client/Assets/Scripts/UI/PlayManager.cs
+++ NeoSports_Client/Assets/Scripts/UI/PlayManager.cs
@@ -2,18 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayManager : Singleton<PlayManager>
 {
 
-	public void StartRopePullGame()
+	public GameObject ropePullMultiButton;
+	public GameObject ropeJumpMultiButton;
+	public GameObject basketBallMultiButton;
+
+	void Start()
+	{
+		DisableMultiPlayButton();
+	}
+
+	void DisableMultiPlayButton()
+	{
+		ropePullMultiButton.GetComponent<Button>().enabled = false;
+		ropeJumpMultiButton.GetComponent<Button>().enabled = false;
+		basketBallMultiButton.GetComponent<Button>().enabled = false;
+	}
+
+	#region Button Event
+	public void StartRopePullSingleGame()
 	{
 		SceneManager.LoadScene(SceneName.RopeGameSceneName);
 	}
 
-	public void StartJumpRopeGame()
+	public void StartRopePullMultiGame()
+	{
+		SceneManager.LoadScene(SceneName.RopeGameSceneName);
+	}
+
+	public void StartJumpRopeSingleGame()
 	{
 		SceneManager.LoadScene(SceneName.JumpRopeGameSceneName);
 	}
+
+	public void StartJumpRopeMultiGame()
+	{
+		SceneManager.LoadScene(SceneName.JumpRopeGameSceneName);
+	}
+
+	public void StartBasketBallSingleGame()
+	{
+		SceneManager.LoadScene(SceneName.BasketBallGameSceneName);
+	}
+
+	public void StartBasketBallMultiGame()
+	{
+		SceneManager.LoadScene(SceneName.BasketBallGameSceneName);
+	}
+	#endregion
 
 }
