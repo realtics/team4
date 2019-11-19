b3d655b2091119e30b88e425367b497537909d17
ath331 <kstnftk@naver.com>
2019-11-19 17:08:10 +0900

    Server_Modify_MultiThread
    멀티쓰레드 부분 수정

--- NeoSports_Server/Server_BOOST/ServerMain/ServerMain.cpp
+++ NeoSports_Server/Server_BOOST/ServerMain/ServerMain.cpp
@@ -1,14 +1,12 @@
 #include "Server.h"
-#include <boost/thread/thread.hpp>
 #include <process.h>
 #include <boost/bind.hpp>
+#include <boost/thread/thread.hpp>
 
 const int MAX_SESSION_COUNT = 100;
 
 int main()
 {
-	std::vector<std::thread> thread_pool{};
-
 	boost::asio::io_context io_service;
 
 	Server server(io_service);
@@ -16,14 +14,16 @@ int main()
 
 	SYSTEM_INFO systemInfo;
 	GetSystemInfo(&systemInfo);
+
 	server.Start();
 
+	boost::thread_group tg;
 	for (int i = 0; i < systemInfo.dwNumberOfProcessors; i++)
 	{
-		boost::thread thread(boost::bind(&boost::asio::io_service::run,
+		tg.create_thread(boost::bind(&boost::asio::io_service::run,
 			&io_service));
-		thread.join();
 	}
+	tg.join_all();
 
 	std::cout << "네트워크 종료" << std::endl;
 
