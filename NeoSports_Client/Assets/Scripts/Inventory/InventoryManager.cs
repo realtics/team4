1eddf75907b4bc57f412318f12739e441a16cd1d
lee jung uk <dlwjddnr93@naver.com>
2019-11-20 15:17:34 +0900

    Client_MenuScene/NetworkManager(클라아이디 결정)
    클라이언트에서 아이디 결정시 서버에 데이터를 보내주게 하였습니다.

--- NeoSports_Client/Assets/Scripts/Inventory/InventoryManager.cs
+++ NeoSports_Client/Assets/Scripts/Inventory/InventoryManager.cs
@@ -6,158 +6,159 @@ using Newtonsoft.Json;
 
 public class InventoryManager : Singleton<InventoryManager>
 {
-	const string CharacterDataName = "CharacterData";
-	const string EquipmentDataName = "EquipmentData";
-	const int InputFieldContent = 2;
-	const int MaxNickName = 8;
+    const string CharacterDataName = "CharacterData";
+    const string EquipmentDataName = "EquipmentData";
+    const int InputFieldContent = 2;
+    const int MaxNickName = 8;
 
-	public GameObject prefCharacterPpiYaGi;
-	public GameObject prefCharacterTurkeyJelly;
-	public GameObject nickNameInputField;
+    public GameObject prefCharacterPpiYaGi;
+    public GameObject prefCharacterTurkeyJelly;
+    public GameObject nickNameInputField;
 
-	#region Property
-	public Dictionary<CharacterInfo.EType, CharacterInfo> CharacterInfos { get; set; }
-	public Dictionary<EquipmentInfo.EType, EquipmentInfo> EquipmentInfos { get; set; }
-	public CharacterInfo CurrentCharacter { get; set; }
-	public EquipmentInfo CurrentEquipment { get; set; }
-	public string PlayerNickName { get; set; }
-	#endregion
+    #region Property
+    public Dictionary<CharacterInfo.EType, CharacterInfo> CharacterInfos { get; set; }
+    public Dictionary<EquipmentInfo.EType, EquipmentInfo> EquipmentInfos { get; set; }
+    public CharacterInfo CurrentCharacter { get; set; }
+    public EquipmentInfo CurrentEquipment { get; set; }
+    public string PlayerNickName { get; set; }
+    #endregion
 
-	void Awake()
-	{
-		if (instance != null)
-		{
-			Destroy(gameObject);
-			return;
-		}
-		DontDestroyOnLoad(this);
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(this);
 
-		CharacterInfos = new Dictionary<CharacterInfo.EType, CharacterInfo>();
-		EquipmentInfos = new Dictionary<EquipmentInfo.EType, EquipmentInfo>();
+        CharacterInfos = new Dictionary<CharacterInfo.EType, CharacterInfo>();
+        EquipmentInfos = new Dictionary<EquipmentInfo.EType, EquipmentInfo>();
 
-		ReadCharacterInfos();
-		ReadEquipmentInfos();
+        ReadCharacterInfos();
+        ReadEquipmentInfos();
 
-		CurrentCharacter = CharacterInfos[CharacterInfo.EType.PpiYaGi];
-		CurrentEquipment = EquipmentInfos[EquipmentInfo.EType.BlackFedora];
-		Singleton<GallaryManager>.Instance.SetPreview();
-		//CreateJson();
+        CurrentCharacter = CharacterInfos[CharacterInfo.EType.PpiYaGi];
+        CurrentEquipment = EquipmentInfos[EquipmentInfo.EType.BlackFedora];
+        Singleton<GallaryManager>.Instance.SetPreview();
+        //CreateJson();
 
-		PlayerNickName = "플레이어";
-	}
+        PlayerNickName = "플레이어";
+    }
 
-	#region Read Infos From Json
-	void ReadCharacterInfos()
-	{
-		CharacterInfo.JsonData[] datas;
+    #region Read Infos From Json
+    void ReadCharacterInfos()
+    {
+        CharacterInfo.JsonData[] datas;
 
-		string dataPath = ResourceManager.JsonDataPath + CharacterDataName;
-		string data = ResourceManager.Instance.ReadJsonDataString(dataPath);
+        string dataPath = ResourceManager.JsonDataPath + CharacterDataName;
+        string data = ResourceManager.Instance.ReadJsonDataString(dataPath);
 
-		datas = JsonConvert.DeserializeObject<CharacterInfo.JsonData[]>(data);
+        datas = JsonConvert.DeserializeObject<CharacterInfo.JsonData[]>(data);
 
-		foreach (var child in datas)
-		{
-			CharacterInfo info = new CharacterInfo(child);
-			CharacterInfos.Add(child.charType, info);
-		}
-	}
+        foreach (var child in datas)
+        {
+            CharacterInfo info = new CharacterInfo(child);
+            CharacterInfos.Add(child.charType, info);
+        }
+    }
 
-	void ReadEquipmentInfos()
-	{
-		EquipmentInfo.JsonData[] datas;
+    void ReadEquipmentInfos()
+    {
+        EquipmentInfo.JsonData[] datas;
 
-		string dataPath = ResourceManager.JsonDataPath + EquipmentDataName;
-		string data = ResourceManager.Instance.ReadJsonDataString(dataPath);
+        string dataPath = ResourceManager.JsonDataPath + EquipmentDataName;
+        string data = ResourceManager.Instance.ReadJsonDataString(dataPath);
 
-		datas = JsonConvert.DeserializeObject<EquipmentInfo.JsonData[]>(data);
+        datas = JsonConvert.DeserializeObject<EquipmentInfo.JsonData[]>(data);
 
-		foreach (var child in datas)
-		{
-			EquipmentInfo info = new EquipmentInfo(child);
-			EquipmentInfos.Add(child.itemType, info);
-		}
-	}
-	#endregion
+        foreach (var child in datas)
+        {
+            EquipmentInfo info = new EquipmentInfo(child);
+            EquipmentInfos.Add(child.itemType, info);
+        }
+    }
+    #endregion
 
-	public void DecideNickName()
-	{
-		Transform mInputFieldText = nickNameInputField.transform.GetChild(InputFieldContent);
-		string inputName = mInputFieldText.GetComponent<Text>().text;
-		if (!IsUseableNickName(inputName))
-		{
-			return;
-		}
+    public void DecideNickName()
+    {
+        Transform mInputFieldText = nickNameInputField.transform.GetChild(InputFieldContent);
+        string inputName = mInputFieldText.GetComponent<Text>().text;
+        if (!IsUseableNickName(inputName))
+        {
+            return;
+        }
 
-		PlayerNickName = inputName;
-		Debug.Log("결정된 닉네임: " + PlayerNickName);
-	}
+        PlayerNickName = inputName;
+        Debug.Log("결정된 닉네임: " + PlayerNickName);
+        NetworkManager.Instance.NotifyNickName(PlayerNickName);
+    }
 
-	bool IsUseableNickName(string name)
-	{
-		if(name.Length == 0)
-		{
-			PopupManager.PopupData pData;
-			pData.text = "최소 1글자 이상은 입력해야 합니다!";
-			pData.okFlag = true;
-			pData.callBack = null;
-			PopupManager.Instance.ShowPopup(pData);
-			return false;
-		}
+    bool IsUseableNickName(string name)
+    {
+        if (name.Length == 0)
+        {
+            PopupManager.PopupData pData;
+            pData.text = "최소 1글자 이상은 입력해야 합니다!";
+            pData.okFlag = true;
+            pData.callBack = null;
+            PopupManager.Instance.ShowPopup(pData);
+            return false;
+        }
 
-		if(name.Length > MaxNickName)
-		{
-			PopupManager.PopupData pData;
-			pData.text = "닉네임은 최대 8글자 까지 가능합니다!";
-			pData.okFlag = true;
-			pData.callBack = null;
-			PopupManager.Instance.ShowPopup(pData);
-			return false;
-		}
+        if (name.Length > MaxNickName)
+        {
+            PopupManager.PopupData pData;
+            pData.text = "닉네임은 최대 8글자 까지 가능합니다!";
+            pData.okFlag = true;
+            pData.callBack = null;
+            PopupManager.Instance.ShowPopup(pData);
+            return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	void CreateJson()
-	{
-		CharacterInfo[] mCharacterInfos = new CharacterInfo[2];
-		CharacterInfo.JsonData[] mCharacterDatas = new CharacterInfo.JsonData[2];
-		mCharacterDatas[0].charType = CharacterInfo.EType.PpiYaGi;
-		mCharacterDatas[0].charName = "삐약이";
-		mCharacterDatas[0].charStat = new Status(1, 1, 1, 1);
-		mCharacterDatas[0].iconName = "Character_PpiYaGi";
-		mCharacterInfos[0] = new CharacterInfo(mCharacterDatas[0]);
+    void CreateJson()
+    {
+        CharacterInfo[] mCharacterInfos = new CharacterInfo[2];
+        CharacterInfo.JsonData[] mCharacterDatas = new CharacterInfo.JsonData[2];
+        mCharacterDatas[0].charType = CharacterInfo.EType.PpiYaGi;
+        mCharacterDatas[0].charName = "삐약이";
+        mCharacterDatas[0].charStat = new Status(1, 1, 1, 1);
+        mCharacterDatas[0].iconName = "Character_PpiYaGi";
+        mCharacterInfos[0] = new CharacterInfo(mCharacterDatas[0]);
 
-		mCharacterDatas[1].charType = CharacterInfo.EType.Unknown;
-		mCharacterDatas[1].charName = "잠김";
-		mCharacterDatas[1].charStat = new Status(0, 0, 0, 0);
-		mCharacterDatas[1].iconName = "Icon_Group_78";
-		mCharacterInfos[1] = new CharacterInfo(mCharacterDatas[1]);
+        mCharacterDatas[1].charType = CharacterInfo.EType.Unknown;
+        mCharacterDatas[1].charName = "잠김";
+        mCharacterDatas[1].charStat = new Status(0, 0, 0, 0);
+        mCharacterDatas[1].iconName = "Icon_Group_78";
+        mCharacterInfos[1] = new CharacterInfo(mCharacterDatas[1]);
 
-		EquipmentInfo[] mEquipmentInfos = new EquipmentInfo[2];
-		EquipmentInfo.JsonData[] mEquipmentDatas = new EquipmentInfo.JsonData[2];
-		mEquipmentDatas[0].itemType = EquipmentInfo.EType.BlackFedora;
-		mEquipmentDatas[0].itemName = "흑색 중절모";
-		mEquipmentDatas[0].itemStatus = new Status(0, 1, 1, 0);
-		mEquipmentDatas[0].iconName = "Equipment_Fedora_0";
-		mEquipmentInfos[0] = new EquipmentInfo(mEquipmentDatas[0]);
-		mEquipmentDatas[1].itemType = EquipmentInfo.EType.BrownFedora;
-		mEquipmentDatas[1].itemName = "갈색 중절모";
-		mEquipmentDatas[1].itemStatus = new Status(1, 0, 0, 1);
-		mEquipmentDatas[1].iconName = "Equipment_Fedora_1";
-		mEquipmentInfos[1] = new EquipmentInfo(mEquipmentDatas[1]);
+        EquipmentInfo[] mEquipmentInfos = new EquipmentInfo[2];
+        EquipmentInfo.JsonData[] mEquipmentDatas = new EquipmentInfo.JsonData[2];
+        mEquipmentDatas[0].itemType = EquipmentInfo.EType.BlackFedora;
+        mEquipmentDatas[0].itemName = "흑색 중절모";
+        mEquipmentDatas[0].itemStatus = new Status(0, 1, 1, 0);
+        mEquipmentDatas[0].iconName = "Equipment_Fedora_0";
+        mEquipmentInfos[0] = new EquipmentInfo(mEquipmentDatas[0]);
+        mEquipmentDatas[1].itemType = EquipmentInfo.EType.BrownFedora;
+        mEquipmentDatas[1].itemName = "갈색 중절모";
+        mEquipmentDatas[1].itemStatus = new Status(1, 0, 0, 1);
+        mEquipmentDatas[1].iconName = "Equipment_Fedora_1";
+        mEquipmentInfos[1] = new EquipmentInfo(mEquipmentDatas[1]);
 
-		string tmpStr = JsonConvert.SerializeObject(mCharacterDatas);
-		Debug.Log(tmpStr);
+        string tmpStr = JsonConvert.SerializeObject(mCharacterDatas);
+        Debug.Log(tmpStr);
 
-		tmpStr = JsonConvert.SerializeObject(mEquipmentDatas);
-		Debug.Log(tmpStr);
+        tmpStr = JsonConvert.SerializeObject(mEquipmentDatas);
+        Debug.Log(tmpStr);
 
-		CharacterInfos.Add(mCharacterInfos[0].Type, mCharacterInfos[0]);
-		CharacterInfos.Add(mCharacterInfos[1].Type, mCharacterInfos[1]);
+        CharacterInfos.Add(mCharacterInfos[0].Type, mCharacterInfos[0]);
+        CharacterInfos.Add(mCharacterInfos[1].Type, mCharacterInfos[1]);
 
-		EquipmentInfos.Add(mEquipmentInfos[0].Type, mEquipmentInfos[0]);
-		EquipmentInfos.Add(mEquipmentInfos[1].Type, mEquipmentInfos[1]);
-	}
+        EquipmentInfos.Add(mEquipmentInfos[0].Type, mEquipmentInfos[0]);
+        EquipmentInfos.Add(mEquipmentInfos[1].Type, mEquipmentInfos[1]);
+    }
 
 }
--- NeoSports_Client/Assets/Scripts/Network/NetworkManager.cs
+++ NeoSports_Client/Assets/Scripts/Network/NetworkManager.cs
@@ -11,22 +11,25 @@ using System;
 
 using Newtonsoft.Json;
 
+enum PACKET_INDEX
+{
+    REQ_IN = 1,
+};
 public struct PACKET_HEADER
 {
     public int packetIndex;
     public int packetSize;
-}
-
-public struct JsonExample
+};
+public struct PACKET_REQ_IN
 {
     public PACKET_HEADER header;
-    public int Data1;
-    public String Data2;
-}
+    public string name;
+};
+
 public struct TempPacket
 {
     public PACKET_HEADER header;
-}
+};
 
 public class NetworkManager : Singleton<NetworkManager>
 {
@@ -44,7 +47,68 @@ public class NetworkManager : Singleton<NetworkManager>
             return;
         }
         DontDestroyOnLoad(this);
+        Connect();
 
+        if (_sock.Connected)
+        {
+            //SendToServerPacket();
+            ReciveFromSeverPacket();
+        }
+    }
+
+    public void NotifyNickName(string playerNickName)
+    {
+        //TO DO : 서버에게 패킷으로 플레이어가 결정한 별명 보내주기 
+        var headerPacket = new PACKET_HEADER { packetIndex = (int)PACKET_INDEX.REQ_IN, packetSize = 10 };
+        var p = new PACKET_REQ_IN { header = headerPacket, name = playerNickName };
+        string json;
+        json = JsonConvert.SerializeObject(p); //객체를 json직렬화 
+        json += '\0';//서버에서 널문자까지 읽기 위해 널문자붙이기
+        byte[] bufSend = new byte[128]; //전송을 위해 바이트단위로 변환
+        bufSend = Encoding.UTF8.GetBytes(json);
+        _sock.Send(bufSend);
+    }
+
+    void SendToServerPacket()
+    {
+        //TODO : 패킷을 제이슨으로 직렬화,역직렬화 시키는 함수 작성하기
+        {
+            var headerPacket = new PACKET_HEADER { packetIndex = 1, packetSize = 10 };
+            var p = new PACKET_REQ_IN { header = headerPacket, name = "aa" };
+            string json;
+            json = JsonConvert.SerializeObject(p); //객체를 json직렬화 
+            json += '\0';//서버에서 널문자까지 읽기 위해 널문자붙이기
+            byte[] bufSend = new byte[128]; //전송을 위해 바이트단위로 변환
+            bufSend = Encoding.UTF8.GetBytes(json);
+            _sock.Send(bufSend);
+        }
+    }
+
+    void ReciveFromSeverPacket()
+    {
+        //byte[] bufRecv = new byte[128]; //수신을 위해 바이트단위로 변환
+        //int n = _sock.Receive(bufRecv);
+        //Debug.Log("recv");
+        //Debug.Log(n);
+
+        ////string recvData = Encoding.UTF8.GetString(bufRecv, 0, n);
+        ////int bufLen = Encoding.Default.GetBytes(bufRecv);
+        //int bufLen = bufRecv.Length;
+        //string recvData = Encoding.UTF8.GetString(bufRecv, 0, n);
+        //Debug.Log(recvData);
+
+        //var data = JsonConvert.DeserializeObject<TempPacket>(recvData);
+        //if (data.header.packetIndex == 101) //JsonExample
+        //{
+        //    var packetTemp = JsonConvert.DeserializeObject<JsonExample>(recvData);
+        //    Debug.Log(packetTemp.Data1);
+        //    Debug.Log(packetTemp.Data2);
+        //}
+
+    }
+
+    void Connect()
+    {
         _sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
         if (_sock == null)
         {
@@ -58,8 +122,8 @@ public class NetworkManager : Singleton<NetworkManager>
         }
         try
         {
-            _sock.Connect(new IPEndPoint(IPAddress.Parse(LoopbackAdress), PortNumber));
-            //_sock.Connect(new IPEndPoint(IPAddress.Parse(IpAdress), PortNumber));        
+            //_sock.Connect(new IPEndPoint(IPAddress.Parse(LoopbackAdress), PortNumber));
+            _sock.Connect(new IPEndPoint(IPAddress.Parse(IpAdress), PortNumber));        
         }
         catch (SocketException se)
         {
@@ -71,53 +135,11 @@ public class NetworkManager : Singleton<NetworkManager>
             PopupManager.Instance.ShowPopup(a);
             return;
         }
-        if (_sock.Connected)
-        {
-            SendToServerPacket();
-            ReciveFromSeverPacket();
-        }
-    }
-
-    void SendToServerPacket()
-    {
-        //TODO : 패킷을 제이슨으로 직렬화,역직렬화 시키는 함수 작성하기
-        {
-            var packetHeader = new PACKET_HEADER { packetIndex = 101, packetSize = 10 };
-            var jsonPacket = new JsonExample { header = packetHeader, Data1 = 2, Data2 = "Hi" };
-            string json;
-            json = JsonConvert.SerializeObject(jsonPacket); //객체를 json직렬화
-            json += '\0'; //서버에서 널문자까지 읽기 위해 널문자붙이기
-            byte[] bufSend = new byte[128]; //전송을 위해 바이트단위로 변환
-            bufSend = Encoding.UTF8.GetBytes(json);
-            _sock.Send(bufSend);
-        }
-    }
-
-    void ReciveFromSeverPacket()
-    {
-        byte[] bufRecv = new byte[128]; //수신을 위해 바이트단위로 변환
-        int n = _sock.Receive(bufRecv);
-        Debug.Log("recv");
-        Debug.Log(n);
-
-        //string recvData = Encoding.UTF8.GetString(bufRecv, 0, n);
-        //int bufLen = Encoding.Default.GetBytes(bufRecv);
-        int bufLen = bufRecv.Length;
-        string recvData = Encoding.UTF8.GetString(bufRecv, 0, n);
-        Debug.Log(recvData);
-
-        var data = JsonConvert.DeserializeObject<TempPacket>(recvData);
-        if (data.header.packetIndex == 101) //JsonExample
-        {
-            var packetTemp = JsonConvert.DeserializeObject<JsonExample>(recvData);
-            Debug.Log(packetTemp.Data1);
-            Debug.Log(packetTemp.Data2);
-        }
-
     }
 
     void ExitProgram()
     {
+        Debug.Log("Call Exit");
         Application.Quit();
     }
 }
