5f71eba385d0436a2580234e62c7a39ea15f946d
Jiheon Kim <rlawys4565@gmail.com>
2019-11-21 18:48:22 +0900

    Client_신규 게임(네오 파밍)
    - 신규 게임 네오 파밍 프로젝트 시작
    - 게임 관련 리소스 파일 추가
    - 몇 가지 스크립트 추가

---
+++ NeoSports_Client/Assets/Scripts/FarmGame/LandManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class LandManager : Singleton<LandManager>
+{
+
+	const int MaxLandWidth = 10;
+	const int MaxLandHeight = 10;
+
+	Dictionary<Point, Land> landDic;
+
+	void Awake()
+	{
+		instance = this;
+
+		for (int i = 0; i < MaxLandWidth; i++)
+		{
+			for (int j = 0; j < MaxLandHeight; j++)
+			{
+				Point pt = new Point(i, j);
+			}
+		}
+	}
+
+	void ResetLands()
+	{
+		foreach (KeyValuePair<Point, Land> item in landDic)
+		{
+
+		}
+	}
+
+
+
+}
