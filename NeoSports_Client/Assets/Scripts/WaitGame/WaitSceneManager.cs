6ac12bd0b8a5b333b936a17f2bad5769a95f8ab5
lee jung uk <dlwjddnr93@naver.com>
2019-11-27 14:42:52 +0900

    Client_WaitScene/WaitSceneManager 대기씬 플레이어 고른 캐릭터 동적생성, 마우스 클릭 한곳으로 움직이기 구현

--- NeoSports_Client/Assets/Scripts/WaitGame/WaitSceneManager.cs
+++ NeoSports_Client/Assets/Scripts/WaitGame/WaitSceneManager.cs
@@ -7,24 +7,37 @@ public class WaitSceneManager : Singleton<WaitSceneManager>
 	// Prefab Character
 	public GameObject ppiYakCharacter;
 	public GameObject turkeyJellyCharacter;
+	//public
+	public float characterSpeed;
+
+	GameObject _Character;
+	Camera _mainCam;
+	Vector2 _targetPos;
 
 	void Awake()
 	{
-		SelectInstantCharacter(InventoryManager.Instance.CurrentCharacter.Type, null);
+		_Character = null;
+		_mainCam = Camera.main;
+		if (InventoryManager.Instance != null)
+			SelectInstantCharacter(InventoryManager.Instance.CurrentCharacter.Type);
+		else
+			SelectInstantCharacter(CharacterInfo.EType.PpiYaGi);
 	}
 
-	void SelectInstantCharacter(CharacterInfo.EType charType, Transform parent)
+	void SelectInstantCharacter(CharacterInfo.EType charType)
 	{
 		switch (charType)
 		{
 			case CharacterInfo.EType.PpiYaGi:
 				{
-					Instantiate(ppiYakCharacter, parent);
+					_Character = new GameObject();
+					Instantiate(ppiYakCharacter, _Character.transform);
 					break;
 				}
 			case CharacterInfo.EType.TurkeyJelly:
 				{
-					Instantiate(turkeyJellyCharacter, parent);
+					_Character = new GameObject();
+					_Character = Instantiate(turkeyJellyCharacter, _Character.transform);
 					break;
 				}
 			default:
@@ -34,4 +47,22 @@ public class WaitSceneManager : Singleton<WaitSceneManager>
 		}
 
 	}
+
+	void Update()
+	{
+		if (Input.GetMouseButtonUp(0) == true) // mobile로 변경시 touch 이벤트 추가.
+		{
+			DecideTargetPos();
+		}
+		if ((Vector2)_Character.transform.position != _targetPos)
+		{
+			_Character.transform.position = Vector2.MoveTowards(_Character.transform.position, _targetPos, characterSpeed * Time.deltaTime);
+		}
+	}
+
+	void DecideTargetPos()
+	{
+		_targetPos =  (Vector2)_mainCam.ScreenToWorldPoint(Input.mousePosition);
+	}
+
 }
