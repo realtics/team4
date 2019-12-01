1e8c74c347fe329c80efff74c25929da370d0c62
lee jung uk <dlwjddnr93@naver.com>
2019-12-01 19:37:04 +0900

    Client_Common/ObjectPool Interface
    오브젝트 풀을 만들수 있는 팩토리 클래스를 구현했습니다.

---
+++ NeoSports_Client/Assets/Scripts/Common/FrameWork/PoolFactory.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolFactory
+{
+    List<RecycleObject> _pool = new List<RecycleObject>();
+    int _defaultPoolSize;
+    RecycleObject _prefab;
+
+    public PoolFactory(RecycleObject prefab, int defaultPoolSize = 5)
+    {
+        this._prefab = prefab;
+        this._defaultPoolSize = defaultPoolSize;
+
+        Debug.Assert(this._prefab != null, "Prefab is null!");
+    }
+
+    void CreatePool()
+    {
+        for (int i = 0; i < _defaultPoolSize; ++i)
+        {
+            RecycleObject obj = GameObject.Instantiate(_prefab) as RecycleObject;
+            obj.gameObject.SetActive(false);
+            _pool.Add(obj);
+        }
+    }
+
+    public RecycleObject Get()
+    {
+        if (_pool.Count == 0)
+        {
+            CreatePool();
+        }
+
+        int lastIndex = _pool.Count - 1;
+        RecycleObject obj = _pool[lastIndex];
+        _pool.RemoveAt(lastIndex);
+        obj.gameObject.SetActive(true);
+        return obj;
+    }
+
+    public void Restore(RecycleObject obj)
+    {
+        Debug.Assert(obj != null, "NUll object to be returned!");
+        obj.gameObject.SetActive(false);
+        _pool.Add(obj);
+    }
+}
