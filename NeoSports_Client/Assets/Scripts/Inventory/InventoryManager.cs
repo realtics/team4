9bdbb0fd905e6b084d84bf13b2f94623214b4c08
Jiheon Kim <rlawys4565@gmail.com>
2019-12-04 14:26:57 +0900

    Client_닉네임 시스템, JsonDll 삭제
    - 더 이상 사용하지 않는 JsonFX.DLL 삭제
    - 메인 메뉴 씬을 로드할때 이미 닉네임을 정했다면 더 이상 닉네임 결정 창이 나오지 않음
    - 안드로이드에서 닉네임 결정을 누를때 에러가 나던 문제 해결

--- NeoSports_Client/Assets/Scripts/Inventory/InventoryManager.cs
+++ NeoSports_Client/Assets/Scripts/Inventory/InventoryManager.cs
@@ -1,20 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using Newtonsoft.Json;
 
 public class InventoryManager : Singleton<InventoryManager>
 {
 	const string CharacterDataName = "CharacterData";
 	const string EquipmentDataName = "EquipmentData";
-	const int InputFieldContent = 2;
-	const int MaxNickName = 8;
 
 	public GameObject prefCharacterPpiYaGi;
 	public GameObject prefCharacterTurkeyJelly;
-	public GameObject nickNameInputGroup;
-	public GameObject nickNameInputField;
 
 	#region Property
 	public Dictionary<CharacterInfo.EType, CharacterInfo> CharacterInfos { get; set; }
@@ -22,6 +17,7 @@ public class InventoryManager : Singleton<InventoryManager>
 	public CharacterInfo CurrentCharacter { get; set; }
 	public EquipmentInfo CurrentEquipment { get; set; }
 	public string PlayerNickName { get; set; }
+	public bool IsNickNameDecide { get; set; }
 	#endregion
 
 	void Awake()
@@ -38,6 +34,7 @@ public class InventoryManager : Singleton<InventoryManager>
 		EquipmentInfos = new Dictionary<EquipmentInfo.EType, EquipmentInfo>();
 
 		PlayerNickName = "플레이어";
+		IsNickNameDecide = false;
 	}
 
 	void Start()
@@ -50,6 +47,8 @@ public class InventoryManager : Singleton<InventoryManager>
 		GallaryManager.Instance.SetPreview();
 	}
 
+	
+
 	#region Read Infos From Json
 	void ReadCharacterInfos()
 	{
@@ -57,7 +56,6 @@ public class InventoryManager : Singleton<InventoryManager>
 
 		string dataPath = ResourceManager.JsonDataPath + CharacterDataName;
 		string dataStr = ResourceManager.Instance.ReadJsonDataString(dataPath);
-		//datas = JsonReader.Deserialize<CharacterInfo.JsonData[]>(dataStr);
 		datas = JsonConvert.DeserializeObject<CharacterInfo.JsonData[]>(dataStr);
 
 		foreach (var child in datas)
@@ -83,46 +81,6 @@ public class InventoryManager : Singleton<InventoryManager>
 		}
 	}
 	#endregion
-
-	public void DecideNickName()
-	{
-		Transform mInputFieldText = nickNameInputField.transform.GetChild(InputFieldContent);
-		string inputName = mInputFieldText.GetComponent<Text>().text;
-		if (!IsUseableNickName(inputName))
-		{
-			return;
-		}
-
-		PlayerNickName = inputName;
-		nickNameInputGroup.SetActive(false);
-		NetworkManager.Instance.SendNickName(PlayerNickName);
-		Debug.Log("결정된 닉네임: " + PlayerNickName);
-	}
-
-	bool IsUseableNickName(string name)
-	{
-		if (name.Length == 0)
-		{
-			PopupManager.PopupData pData;
-			pData.text = "최소 1글자 이상은 입력해야 합니다!";
-			pData.okFlag = true;
-			pData.callBack = null;
-			PopupManager.Instance.ShowPopup(pData);
-			return false;
-		}
-
-		if (name.Length > MaxNickName)
-		{
-			PopupManager.PopupData pData;
-			pData.text = "닉네임은 최대 8글자 까지 가능합니다!";
-			pData.okFlag = true;
-			pData.callBack = null;
-			PopupManager.Instance.ShowPopup(pData);
-			return false;
-		}
-
-		return true;
-	}
 
 	//void CreateJson()
 	//{
---
+++ NeoSports_Client/Assets/Scripts/Inventory/NickNameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MainMenu
+{
+	public class NickNameManager : Singleton<NickNameManager>
+	{
+		const int MaxNickName = 8;
+
+		public GameObject nickNameInputGroup;
+		public GameObject nickNameInputText;
+
+		void Start()
+		{
+			if (InventoryManager.Instance.IsNickNameDecide)
+			{
+				nickNameInputGroup.SetActive(false);
+			}
+		}
+
+		public void DecideNickName()
+		{
+			string inputName = nickNameInputText.transform.GetComponent<Text>().text;
+			if (!IsUseableNickName(inputName))
+			{
+				return;
+			}
+
+			InventoryManager.Instance.PlayerNickName = inputName;
+			nickNameInputGroup.SetActive(false);
+			NetworkManager.Instance.SendNickName(inputName);
+			InventoryManager.Instance.IsNickNameDecide = true;
+			Debug.Log("결정된 닉네임: " + inputName);
+		}
+
+		bool IsUseableNickName(string name)
+		{
+			if (name.Length == 0)
+			{
+				PopupManager.PopupData pData;
+				pData.text = "최소 1글자 이상은 입력해야 합니다!";
+				pData.okFlag = true;
+				pData.callBack = null;
+				PopupManager.Instance.ShowPopup(pData);
+				return false;
+			}
+
+			if (name.Length > MaxNickName)
+			{
+				PopupManager.PopupData pData;
+				pData.text = "닉네임은 최대 8글자 까지 가능합니다!";
+				pData.okFlag = true;
+				pData.callBack = null;
+				PopupManager.Instance.ShowPopup(pData);
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
