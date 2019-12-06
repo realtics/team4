3b4536cbf815ca8f32682588afb37e4fb20c2a85
lee jung uk <dlwjddnr93@naver.com>
2019-12-06 13:15:05 +0900

    Client_Common/PlayerController
    캐릭터와 플레이어컨트롤러 리팩토링 중입니다.

--- NeoSports_Client/Assets/Scripts/Common/Controller/PlayerController.cs
+++ NeoSports_Client/Assets/Scripts/Common/Controller/PlayerController.cs
@@ -1,18 +1,127 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+enum eControlScene
+{
+	NotControl,
+	PullRope,
+	JumpRoPe,
+	WaitRoom,
+	BaksetBall,
+}
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+	Character _controlChar;
+	eControlScene _controlScene;
+	void InitController(Character controlCharacter)
+	{
+		_controlChar = controlCharacter;
+		SetControlScene(SceneManager.GetActiveScene().name);
+	}
+
+	void Update() // To Do. Update 방식에서 이벤트 방식으로 추후 리팩토링
+	{
+		if (_controlChar == null)
+			return;
+		if (_controlScene == eControlScene.NotControl)
+			return;
+		ProcessInput();
+	}
+
+	void ProcessInput()
+	{
+		switch (_controlScene)
+		{
+			case eControlScene.BaksetBall:
+				{
+					ProcessBasket();
+					break;
+				}
+			case eControlScene.JumpRoPe:
+				{
+					ProcessJumpRope();
+					break;
+				}
+			case eControlScene.PullRope:
+				{
+					ProcessPullRope();
+					break;
+				}
+			case eControlScene.WaitRoom:
+				{
+					ProcessWaitRoom();
+					break;
+				}
+			default:
+				break;
+		}
+	}
+
+	#region Input Case
+	void ProcessBasket()
+	{
+		
+	}
+
+	void ProcessJumpRope()
+	{
+		
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void ProcessPullRope()
+	{ 
+
+	}
+
+	void ProcessWaitRoom()
+	{
+		
+	}
+
+	#endregion
+
+	#region SetSceneEnumFromString
+	void SetControlScene(string sceneName)
+	{
+		if (sceneName.CompareTo(SceneName.MenuSceneName) == 0)
+		{
+			_controlScene = eControlScene.NotControl;
+			return;
+		}
+		else if (sceneName.CompareTo(SceneName.NetworkBasketBallSceneName) == 0)
+		{
+			_controlScene = eControlScene.BaksetBall;
+			return;
+		}
+		else if (sceneName.CompareTo(SceneName.BasketBallGameSceneName) == 0)
+		{
+			_controlScene = eControlScene.BaksetBall;
+			return;
+		}
+		else if (sceneName.CompareTo(SceneName.NetworkRopeGameSceneName) == 0)
+		{
+			_controlScene = eControlScene.PullRope;
+			return;
+		}
+		else if (sceneName.CompareTo(SceneName.RopeGameSceneName) == 0)
+		{
+			_controlScene = eControlScene.PullRope;
+			return;
+		}
+		else if (sceneName.CompareTo(SceneName.JumpRopeGameSceneName) == 0)
+		{
+			_controlScene = eControlScene.JumpRoPe;
+			return;
+		}
+		else if (sceneName.CompareTo(SceneName.WaitGameSceneName) == 0)
+		{
+			_controlScene = eControlScene.WaitRoom;
+			return;
+		}
+		_controlScene = eControlScene.NotControl;
+		return;
+	}
+	#endregion
 }
---
+++ NeoSports_Client/Assets/Scripts/Common/Player/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	Character _character;
+	PlayerController _playerController;
+
+	void InitPlayer(Character character, PlayerController controller)
+	{
+		_character = character;
+		_playerController = controller;
+	}
+
+	
+}
