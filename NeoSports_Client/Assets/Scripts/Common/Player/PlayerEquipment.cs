4687a72fe6c8d6e34f31bd8e28dc15cb738bf30c
lee jung uk <dlwjddnr93@naver.com>
2019-12-12 18:44:29 +0900

    Client_Common/PlayerEquipment
    Player 치장아이템 외관을 관리하는 클래스 틀을 만들었습니다.
    기능 및 -> 인스턴스화는 아직 진행하지 않았습니다.

---
+++ NeoSports_Client/Assets/Scripts/Common/Player/PlayerEquipment.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEquipment : MonoBehaviour
+{
+	Player _ownPlayer;
+	SpriteRenderer _equipSprite;
+	public void InitializeEquipItem(Player player)
+	{
+		if (InventoryManager.Instance == null)
+			return;
+		EquipmentInfo equipInfo = InventoryManager.Instance.CurrentEquipment;
+		_ownPlayer = player;
+
+		transform.parent = player.transform;
+		_equipSprite = gameObject.AddComponent<SpriteRenderer>();
+		_equipSprite.sprite = equipInfo.IconSprite;
+		//transform.localPosition = new Vector3(player.transform.position.y + 1);
+	}
+}
