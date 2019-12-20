6cc1d3bc11b4d3eecb77bf608bf7f43b014e1ac8
Jiheon Kim <rlawys4565@gmail.com>
2019-12-20 18:30:04 +0900

    Client_Common 비동기 로딩 씬 변경
    - 다른 씬을 로딩할 때에도 사용할 수 있도록 구조 변경

--- NeoSports_Client/Assets/Scripts/LoadingSceneManager.cs
+++ NeoSports_Client/Assets/Scripts/LoadingSceneManager.cs
@@ -16,11 +16,16 @@ public class LoadingSceneManager : MonoBehaviour
 
 	private void Start()
 	{
-		if (nextScene == SceneName.FriendFarmSceneName)
+		switch (nextScene)
 		{
-			Instantiate(prefFriendFarmManager);
+			case SceneName.FriendFarmSceneName:
+				Instantiate(prefFriendFarmManager);
+				StartCoroutine(FriendFarmLoadScene());
+				break;
+			default:
+				StartCoroutine(DefaultLoadScene());
+				break;
 		}
-		StartCoroutine(LoadScene());
 	}
 
 	public static void LoadScene(string sceneName)
@@ -29,9 +34,8 @@ public class LoadingSceneManager : MonoBehaviour
 		SceneManager.LoadScene(SceneName.FriendFarmLoadingSceneName);
 	}
 
-	IEnumerator LoadScene()
+	IEnumerator DefaultLoadScene()
 	{
-		Debug.Log("Async Load Scene Start!");
 		yield return null;
 
 		AsyncOperation op = SceneManager.LoadSceneAsync(nextScene);
@@ -65,7 +69,43 @@ public class LoadingSceneManager : MonoBehaviour
 				}
 			}
 		}
-		Debug.Log("Async Load Scene End!");
+	}
+
+	IEnumerator FriendFarmLoadScene()
+	{
+		yield return null;
+
+		AsyncOperation op = SceneManager.LoadSceneAsync(nextScene);
+		op.allowSceneActivation = false;
+
+		float timer = 0.0f;
+
+		while (!op.isDone)
+		{
+			yield return null;
+
+			timer += Time.deltaTime;
+
+			if (op.progress < 0.9f)
+			{
+				progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, op.progress, timer);
+				if (progressBar.fillAmount >= op.progress)
+				{
+					timer = 0f;
+				}
+			}
+			else
+			{
+				progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, 0.9f, timer);
+
+				if (progressBar.fillAmount == 0.9f && FriendFarmManager.Instance.IsDataLoaded)
+				{
+					progressBar.fillAmount = 1.0f;
+					op.allowSceneActivation = true;
+					yield break;
+				}
+			}
+		}
 
 	}
 
