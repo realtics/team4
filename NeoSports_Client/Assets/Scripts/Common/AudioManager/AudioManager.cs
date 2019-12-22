dd14b1b379f1bf8bc7dbdc17c9db163db2f15fc7
lee jung uk <dlwjddnr93@naver.com>
2019-12-22 23:31:04 +0900

    Client_Common/AudioManager_withinScriptableObject
    스크립터블 오브젝트를 사용하여 오디오매니저를 만들었습니다
    => 코드만 작성 사용하기 앞서 테스트 필요.

---
+++ NeoSports_Client/Assets/Scripts/Common/AudioManager/AudioManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    [SerializeField]
+    AudioStorage soundStorage;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(this);
+
+    }
+
+    public void PlaySound(eSoundId id)
+    {
+        AudioSource.PlayClipAtPoint(soundStorage.Get(id), Vector3.zero);
+    }
+}
---
+++ NeoSports_Client/Assets/Scripts/Common/AudioManager/AudioStorage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class AudioStorage : ScriptableObject
+{
+    [SerializeField]
+    SoundSrc[] soundSrcs;
+
+    Dictionary<eSoundId, AudioClip> dicSounds = new Dictionary<eSoundId, AudioClip>();
+
+    void GenerateDictionary()
+    {
+        for (int i = 0; i < soundSrcs.Length; i++)
+        {
+            dicSounds.Add(soundSrcs[i].Id, soundSrcs[i].SoundFile);
+        }
+    }
+
+    public AudioClip Get(eSoundId id)
+    {
+        Debug.Assert(soundSrcs.Length > 0, "No soundSource data!");
+        if (dicSounds.Count == 0)
+        {
+            GenerateDictionary();
+        }
+
+        return dicSounds[id];
+    }
+}
+
+[Serializable]
+public struct SoundSrc
+{
+    [SerializeField]
+    AudioClip soundFile;
+
+    [SerializeField]
+    eSoundId soundId;
+
+    public AudioClip SoundFile { get { return soundFile; } }
+    public eSoundId Id { get { return soundId; } }
+}
+
+public enum eSoundId
+{
+    Shoot, Walk, Start, GameEnd,
+}
